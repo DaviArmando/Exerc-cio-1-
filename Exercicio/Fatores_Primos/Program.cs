0ff6824a8df9fc23ee1830c90a5d22f685ad26f4
Daviarmando <davibradock1@gmail.com>
2022-08-03 20:05:47 -0300

    Desenvolvimento do projeto Fatores_Primos

---
+++ Exercicio/Fatores_Primos/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Digite um número");
+var numero_informado = Console.ReadLine();
+var numero = Convert.ToInt32(numero_informado);
+var fator = 2;
+Console.Write($"{numero} = ");
+while (numero > 1)
+{
+    var e_divisivel = (numero % fator == 0);
+    if (e_divisivel)
+    {
+        Console.Write($"{fator} x ");
+        numero = numero / fator;
+    }
+    else
+        fator++;
+}
+Console.Write($"1");
