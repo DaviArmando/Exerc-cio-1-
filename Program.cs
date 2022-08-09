407d82969659580b2ea2a3aa79c7f5277b1993b0
Daviarmando <110180444+DaviArmando@users.noreply.github.com>
2022-08-09 10:47:44 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Digite o ano: ");
+var anoInformado = Console.ReadLine();
+var ano = Convert.ToInt32(anoInformado);
+
+if (ano % 4 == 0 && ano % 400 == 0)
+{
+    Console.Write("Esse ano é bissexto");
+}
+else
+{
+    Console.Write("Esse ano não é bissexto");
+}
